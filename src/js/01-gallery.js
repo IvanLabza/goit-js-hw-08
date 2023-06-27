a91cceaa340417f97047bd6cf8e438e01fe70adc
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-06-27 18:22:48 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,47 @@
-// Import the SimpleLightbox library
-import SimpleLightbox from 'simplelightbox';
-// Import the additional styles
-import 'simplelightbox/dist/simple-lightbox.min.css';
-// Import the gallery items
-import { galleryItems } from './gallery-items.js';
-// Function to create the gallery markup
-function createGalleryMarkup(items) {
+// Додаємо необхідні імпорти
+import { galleryItems } from './gallery-items'; // Імпортуємо об'єкт galleryItems з файлу gallery-items
+import SimpleLightbox from 'simplelightbox'; // Імпортуємо модуль SimpleLightbox
+import 'simplelightbox/dist/simple-lightbox.min.css'; // Імпортуємо стилі для SimpleLightbox
+
+// Ініціалізуємо змінну list, що посилається на елемент з класом 'gallery'
+const list = document.querySelector('.gallery');
+
+// Функція markup приймає список елементів і повертає HTML-розмітку для кожного елементу
+function markup(items) {
   return items
-    .map(({ preview, original, description }) => {
-      return `
-        <li class="gallery__item">
-          <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-          </a>
-        </li>
-      `;
-    })
+    .map(
+      item => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`
+    )
     .join('');
 }
-// Get the gallery element
-const gallery = document.querySelector('.gallery');
-// Set the gallery markup
-gallery.innerHTML = createGalleryMarkup(galleryItems);
-// Initialize the SimpleLightbox plugin
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
+
+// Додаємо розмітку до нашого списку
+list.insertAdjacentHTML('beforeend', markup(galleryItems));
+
+// Ініціалізуємо новий екземпляр SimpleLightbox з відповідними налаштуваннями
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250, // Задержка перед відображенням підпису
+  captionsData: 'alt', // Дані для підпису беруться з атрибута 'alt'
+  captionPosition: 'bottom', // Позиція підпису - знизу
 });
+
+// Змінюємо стиль списку
+list.style.listStyle = 'none';
+
+// Ініціалізуємо змінну pageLink, що посилається на перший елемент 'a' на сторінці
+const pageLink = document.querySelector('a');
+// Змінюємо стилі для посилання
+pageLink.style.fontWeight = '600';
+pageLink.style.color = 'blue';
+pageLink.style.textDecoration = 'none';
+pageLink.style.display = 'flex';
+pageLink.style.justifyContent = 'end';
+pageLink.style.paddingRight = '20px';
+// Задаємо атрибут href для посилання
+pageLink.href = '02-video.html';
+// Задаємо текст для посилання
+pageLink.textContent = 'Go To The Next Task - VIDEO';
