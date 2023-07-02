a5b34df44c0165185720bdf062b0f9d8046f69fe
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2023-07-02 22:19:22 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,47 +1,27 @@
-// Додаємо необхідні імпорти
-import { galleryItems } from './gallery-items'; // Імпортуємо об'єкт galleryItems з файлу gallery-items
-import SimpleLightbox from 'simplelightbox'; // Імпортуємо модуль SimpleLightbox
-import 'simplelightbox/dist/simple-lightbox.min.css'; // Імпортуємо стилі для SimpleLightbox
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// Ініціалізуємо змінну list, що посилається на елемент з класом 'gallery'
-const list = document.querySelector('.gallery');
 
-// Функція markup приймає список елементів і повертає HTML-розмітку для кожного елементу
-function markup(items) {
-  return items
-    .map(
-      item => `<li class="gallery__item">
-    <a class="gallery__link" href="${item.original}">
-       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-    </a>
- </li>`
-    )
-    .join('');
-}
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-// Додаємо розмітку до нашого списку
-list.insertAdjacentHTML('beforeend', markup(galleryItems));
+const gallaryList = document.querySelector('.gallery');
 
-// Ініціалізуємо новий екземпляр SimpleLightbox з відповідними налаштуваннями
-new SimpleLightbox('.gallery a', {
-  captionDelay: 250, // Задержка перед відображенням підпису
-  captionsData: 'alt', // Дані для підпису беруться з атрибута 'alt'
-  captionPosition: 'bottom', // Позиція підпису - знизу
+const markup = galleryItems
+.map((item) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+<img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>`).join("");
+gallaryList.innerHTML = markup;
+
+gallaryList.style.listStyle = 'none'
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+    spinner: true,
 });
 
-// Змінюємо стиль списку
-list.style.listStyle = 'none';
-
-// Ініціалізуємо змінну pageLink, що посилається на перший елемент 'a' на сторінці
-const pageLink = document.querySelector('a');
-// Змінюємо стилі для посилання
-pageLink.style.fontWeight = '600';
-pageLink.style.color = 'blue';
-pageLink.style.textDecoration = 'none';
-pageLink.style.display = 'flex';
-pageLink.style.justifyContent = 'end';
-pageLink.style.paddingRight = '20px';
-// Задаємо атрибут href для посилання
-pageLink.href = '02-video.html';
-// Задаємо текст для посилання
-pageLink.textContent = 'Go To The Next Task - VIDEO';
+console.log(galleryItems);
